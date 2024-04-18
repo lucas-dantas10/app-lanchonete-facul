f4722a7ea99bd02fa2d5fd80b9c039bc92024c85
vitorvts <vitorgguimaraes56@gmail.com>
2024-04-18 16:54:32 -0300

    [FEAT]:importado o react junto ao useState

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,45 +1,72 @@
-import React from 'react';
-import {useState} from 'react'
-import LoginPNG from '../../../assets/LogoLogin.png'
-import { StyleSheet, Text, View,SafeAreaView,TextInput,TouchableOpacity,Image} from 'react-native';
- 
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import LoginPNG from "../../../assets/LogoLogin.png";
+
 function LoginScreen() {
-  const [inputLogin,onChangeUsuario] = useState('');
-  const [inputSenha,onChangeSenha] = useState('');
-  
+  const [inputUsuario, onChangeUsuario] = useState("");
+  const [inputSenha, onChangeSenha] = useState("");
+
   return (
-    
     <View style={styles.container}>
       <Image style={styles.LoginPNG} source={LoginPNG} />
-        <TextInput style={styles.input} placeholder='Usuário' onChangeText={onChangeUsuario} value={inputLogin}/>
-        <TextInput style={styles.input} placeholder='Senha'   onChangeText={onChangeSenha} value={inputSenha}/>
-        <TouchableOpacity style={styles.buttonLogin}>
-          <Text>Login</Text>
-        </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        placeholder="Usuário"
+        onChangeText={onChangeUsuario}
+        value={inputUsuario}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        onChangeText={onChangeSenha}
+        value={inputSenha}
+      />
+      <TouchableOpacity style={styles.buttonLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,padding:24,backgroundColor:'#fff',alignItems: 'center', 
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
-  
-  input:{
-    width:'90%',margin:12,borderRadius:16,padding: 14,backgroundColor:'#e3e3e3'
-  },
-  buttonLogin:{
-    alignItems: 'center', justifyContent: 'center',
-    width:'90%',margin:12,borderRadius:16,padding:20 ,backgroundColor:'#ffbc0d'
-  },
-  LoginPNG:{
-    width:'100%',
-    height:'30%',
-    marginBottom:'10%',
-    marginTop:'20%',
-    resizeMode: 'contain',
-  }
 
-})
+  input: {
+    width: "90%",
+    margin: 12,
+    borderRadius: 16,
+    padding: 14,
+    backgroundColor: "#e3e3e3",
+  },
+  buttonLogin: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    margin: 12,
+    borderRadius: 16,
+    padding: 20,
+    backgroundColor: "#ffbc0d",
+  },
+  LoginPNG: {
+    width: "100%",
+    height: "30%",
+    marginBottom: "10%",
+    marginTop: "20%",
+    resizeMode: "contain",
+  },
+});
 
 export default LoginScreen;
