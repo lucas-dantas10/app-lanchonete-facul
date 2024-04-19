593ceb77e8a3b7c2f65d429d7166aea4f6ba268b
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-04-18 21:29:14 -0300

    [FEAT] criado tab bars e view de Home para clientes

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
-// In App.js in a new project
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/views/Login/LoginScreen.js";
+import HomeClientScreen from "./src/views/Home/Client/HomeClientScreen.js";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/views/Login/Login.js';
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        {/* <Stack.Screen name="Cadastro" component={CadastroScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const [isLogged, setIsLogged] = useState(true);
+
+    return isLogged ? (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Início" component={HomeClientScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    ) : (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                {/* <Stack.Screen name="Cadastro" component={CadastroScreen} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
---
+++ src/views/Home/Client/HomeClientScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomeClientScreen() {
+    return (
+        <h1>HOME CLIENT</h1>
+    );
+}
+
+export default HomeClientScreen;
