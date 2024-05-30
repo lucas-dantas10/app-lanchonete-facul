2f97f8aae28ef195d61470062e5c26143391e1c0
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-05-30 17:20:34 -0300

    feat: add esboço da tela de profile

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
 import SignUpScreen from "./src/views/SignUp/SignUpScreen.js";
 import CartScreen from "./src/views/Cart/CartScreen.js";
+import ProfileScreen from "./src/views/Profile/ProfileScreen.js";
 import { Buffer } from 'buffer'
 
 window.Buffer = window.Buffer ?? Buffer
@@ -36,7 +37,7 @@ function App() {
 
                     <Tab.Screen
                         name="Perfil"
-                        component={HomeClientScreen}
+                        component={ProfileScreen}
                         options={{
                             tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
                         }}
@@ -69,7 +70,7 @@ function App() {
 
                     <Tab.Screen
                         name="Perfil"
-                        component={HomeClientScreen}
+                        component={ProfileScreen}
                         options={{
                             tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
                         }}
---
+++ src/views/Profile/ProfileScreen.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native-web";
+
+function ProfileScreen() {
+    return (
+        <Text>TESTE</Text>
+    )
+}
+
+export default ProfileScreen;
