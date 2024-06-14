093cc464ac83cdc2acbb502321b28fd438d127ab
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-14 16:48:58 -0300

    [FEAT]:Adicionado o scroll para que o usuario consiga selecionar os inputs adequadamente na tela de login

--- src/views/Login/LoginScreen.js
+++ src/views/Login/LoginScreen.js
@@ -7,121 +7,127 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  Alert 
+  Alert,
 } from "react-native";
 import LoginPNG from "../../../assets/LogoLogin.png";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-function LoginScreen({navigation, route}) {
+function LoginScreen({ navigation, route }) {
   const [inputUsuario, onChangeUsuario] = useState("");
   const [inputSenha, onChangeSenha] = useState("");
-  const [showLoginPng, setShowLoginPng] = useState(true);
-  
+
   async function login() {
     try {
-        const response = await api.post('/login', { email: inputUsuario, password: inputSenha });
-        const { user, token } = response.data;
+      const response = await api.post("/login", {
+        email: inputUsuario,
+        password: inputSenha,
+      });
+      const { user, token } = response.data;
 
-        await AsyncStorage.setItem('user', JSON.stringify(user));
-        await AsyncStorage.setItem('token', token);
+      await AsyncStorage.setItem("user", JSON.stringify(user));
+      await AsyncStorage.setItem("token", token);
 
-        route.params.isLogado(true);
-        route.params.isAdmin(false);
+      route.params.isLogado(true);
+      route.params.isAdmin(false);
 
-        if (user.is_admin == true) {
-            route.params.isAdmin(true);
-        }
-      } catch (error) {
-        Alert.alert('Erro', 'Email ou Senha incorretos!', [
-            {
-              text: 'Cancel',
-              onPress: () => console.log('Cancel Pressed'),
-              style: 'cancel',
-            },
-            {text: 'OK', onPress: () => console.log('OK Pressed')},
-          ]);
-      } finally {
-        navigation.navigate("Home");
+      if (user.is_admin) {
+        route.params.isAdmin(true);
       }
+    } catch (error) {
+      Alert.alert("Erro", "Email ou Senha incorretos!", [
+        {
+          text: "OK",
+          onPress: () => console.log("OK Pressed"),
+        },
+      ]);
+    } finally {
+      navigation.navigate("Home");
+    }
   }
 
   return (
-    <View style={styles.container}>
-    {showLoginPng && <Image style={styles.LoginPNG} source={LoginPNG} />}
-    <TextInput
-      style={styles.input}
-      placeholder="Email"
-      onChangeText={(text) => {
-        onChangeUsuario(text);
-        setShowLoginPng(false); 
-      }}
-      value={inputUsuario}
-    />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        secureTextEntry={true}
-        onChangeText={onChangeSenha}
-        value={inputSenha}
-      />
+    <KeyboardAwareScrollView
+      style={{ flex: 1, backgroundColor: "#fff" }}
+      contentContainerStyle={styles.container}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.inner}>
+        <Image style={styles.LoginPNG} source={LoginPNG} resizeMode="fill" />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => onChangeUsuario(text)}
+          value={inputUsuario}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          secureTextEntry={true}
+          onChangeText={(text) => onChangeSenha(text)}
+          value={inputSenha}
+        />
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Cadastro")}
-        style={styles.buttonRegister}>
-        <Text
-        style={styles.textRegister}>
-          Não Posui uma Conta? <Text style={{color: 'blue'}}>Cadastre-se</Text> 
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-        disabled={inputUsuario != '' && inputSenha != '' ? false : true}
-        onPress={login}
-        style={styles.buttonLogin}
-      >
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Cadastro")}
+          style={styles.buttonRegister}
+        >
+          <Text style={styles.textRegister}>
+            Não Possui uma Conta?{" "}
+            <Text style={{ color: "blue" }}>Cadastre-se</Text>
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          disabled={inputUsuario !== "" && inputSenha !== ""}
+          onPress={login}
+          style={styles.buttonLogin}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: "#fff",
+    flexGrow: 1,
+    justifyContent: "center",
     alignItems: "center",
   },
-
+  inner: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   input: {
     width: "90%",
-    margin: 12,
+    marginVertical: 10,
     borderRadius: 16,
     padding: 14,
     backgroundColor: "#e3e3e3",
   },
-  buttonRegister:{
-    margin: 4,
+  buttonRegister: {
+    marginVertical: 4,
   },
-  textRegister:{
-    color:"#3a3a3a",
+  textRegister: {
+    color: "#3a3a3a",
   },
-
   buttonLogin: {
     alignItems: "center",
     justifyContent: "center",
     width: "90%",
-    margin: 12,
+    marginVertical: 10,
     borderRadius: 16,
     padding: 20,
     backgroundColor: "#ffbc0d",
   },
-
   LoginPNG: {
-    width: "100%",
-    height: "30%",
-    marginBottom: "10%",
-    marginTop: "20%",
-    resizeMode: "contain",
+    width: "60%",
+    height: 240,
+    position: "relative", // Altura fixa da imagem (ajuste conforme necessário para preencher a tela inicialmente)
   },
 });
 
--- src/views/Profile/ProfileScreen.js
+++ src/views/Profile/ProfileScreen.js
@@ -1,67 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Image } from 'react-native-web';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-function ProfileScreen() {
-
-    return (
-        <View style={styles.container}>
-
-           <View>
-                <text>Nome:</text>
-                <text style={styles.nome}>Colocar o nome aqui</text>
-           </View>
-
-           <View>
-                <text>Email:</text>
-                <text style={styles.email}>Colocar o email aqui</text>
-           </View>
-
-           <View>
-                <text>Pedidos:</text>
-                <text style={styles.pedido}>colocar os pedidos aqui</text>
-           </View>
-
-           <View style={styles.sair}>
-                <text>Sair</text>
-           </View>
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://via.placeholder.com/150' }} // URL da imagem de perfil (substitua pela sua própria imagem)
+        style={styles.profileImage}
+      />
+      <Text style={styles.username}>Usuário</Text>
+      <View style={styles.detailsContainer}>
+        <View style={styles.detailItem}>
+          <Text style={styles.detailLabel}>Escola</Text>
+          <Text style={styles.detailValue}>Escola1</Text>
         </View>
-    );
-}
+      </View>
+      <View>
+        <Text style={styles.orderLabel}>Pedido Atual</Text>
+        <Text style = {styles.orderContent}></Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 24,
-        backgroundColor: "#fff",
-        alignItems: "center",
-    },
-    nome:{
-        backgroundColor: "gray",
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 15,
-    },
-    email:{
-        backgroundColor: "gray",
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 15,
-    },
-    pedido:{
-        backgroundColor: "gray",
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 15,
-    },
-    sair:{
-        backgroundColor: "red",
-        borderRadius: 5,
-        padding: 10,
-        fontWeight: "bold",
-        fontSize: 18,
-        width: 200,
-        textAlign: "center",
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  username: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  detailsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  detailItem: {
+    alignItems: 'center',
+  },
+  detailLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  detailValue: {
+    fontSize: 14,
+    color: '#555',
+  },
 });
 
 export default ProfileScreen;
