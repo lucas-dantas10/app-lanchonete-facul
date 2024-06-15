64a2d5dba1432764f6368f077bd0887767c5a6a1
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-06-15 15:30:42 -0300

    refactor: melhoria na tela de perfil

--- src/views/Profile/ProfileScreen.js
+++ src/views/Profile/ProfileScreen.js
@@ -8,16 +8,18 @@ const ProfileScreen = () => {
         source={{ uri: 'https://via.placeholder.com/150' }} // URL da imagem de perfil (substitua pela sua própria imagem)
         style={styles.profileImage}
       />
-      <Text style={styles.username}>Usuário</Text>
+      <Text style={styles.username}>Juvenal</Text>
+
       <View style={styles.detailsContainer}>
         <View style={styles.detailItem}>
           <Text style={styles.detailLabel}>Escola</Text>
-          <Text style={styles.detailValue}>Escola1</Text>
+          <Text style={styles.detailValue}>Santa Barbara</Text>
         </View>
       </View>
-      <View>
+
+      <View style={styles.orderContainer}>
         <Text style={styles.orderLabel}>Pedido Atual</Text>
-        <Text style = {styles.orderContent}></Text>
+        <Text style={styles.orderContent}>Nenhum pedido no momento</Text>
       </View>
     </View>
   );
@@ -28,6 +30,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
   },
   profileImage: {
     width: 150,
@@ -38,27 +42,52 @@ const styles = StyleSheet.create({
   username: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  description: {
-    fontSize: 16,
-    textAlign: 'center',
     marginBottom: 20,
   },
   detailsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
     width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+    marginBottom: 20,
   },
   detailItem: {
-    alignItems: 'center',
+    marginBottom: 10,
   },
   detailLabel: {
     fontSize: 16,
     fontWeight: 'bold',
+    marginBottom: 5,
   },
   detailValue: {
     fontSize: 14,
+    color: '#555',
+  },
+  orderContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  orderLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  orderContent: {
+    fontSize: 16,
     color: '#555',
   },
 });
