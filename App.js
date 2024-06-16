9f5088985b9ea0eee25e4cec26daf0a63d4b0dfa
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-06-16 12:13:48 -0300

    refactor: melhoria na tela principal

--- App.js
+++ App.js
@@ -1,88 +1,102 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./src/views/Login/LoginScreen.js";
-import HomeClientScreen from "./src/views/Home/Client/HomeClientScreen.js";
-import HomeAdminScreen from "./src/views/Home/Admin/HomeAdminScreen.js";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from "@expo/vector-icons";
+import { Buffer } from 'buffer';
+
+import LoginScreen from "./src/views/Login/LoginScreen.js";
 import SignUpScreen from "./src/views/SignUp/SignUpScreen.js";
+import HomeClientScreen from "./src/views/Home/Client/HomeClientScreen.js";
+import HomeAdminScreen from "./src/views/Home/Admin/HomeAdminScreen.js";
 import CartScreen from "./src/views/Cart/CartScreen.js";
 import ProfileScreen from "./src/views/Profile/ProfileScreen.js";
-import { Buffer } from 'buffer'
 
-window.Buffer = window.Buffer ?? Buffer
+// Definindo o Buffer globalmente se não estiver definido (para compatibilidade)
+if (typeof window !== 'undefined') {
+    window.Buffer = window.Buffer || Buffer;
+}
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+function AuthNavigator({ setIsLogged, setIsAdmin }) {
+    return (
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                initialParams={{ isLogado: setIsLogged, isAdmin: setIsAdmin }}
+            />
+            <Stack.Screen name="Cadastro" component={SignUpScreen} />
+        </Stack.Navigator>
+    );
+}
+
+function ClientTabNavigator() {
+    return (
+        <Tab.Navigator
+            screenOptions={{ headerShown: false, activeTintColor: "#808080", inactiveTintColor: "#FFD700" }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeClientScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Feather name="home" color={color} size={size} />,
+                }}
+            />
+            <Tab.Screen
+                name="Carrinho"
+                component={CartScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Feather name="shopping-cart" color={color} size={size} />,
+                }}
+            />
+            <Tab.Screen
+                name="Perfil"
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
+function AdminTabNavigator() {
+    return (
+        <Tab.Navigator
+            screenOptions={{ headerShown: false, activeTintColor: "#808080", inactiveTintColor: "#FFD700" }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeAdminScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Feather name="home" color={color} size={size} />,
+                }}
+            />
+            <Tab.Screen
+                name="Perfil"
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
 
 function App() {
     const [isLogged, setIsLogged] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
 
-    return isLogged ? (
-        isAdmin ? (
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={{ headerShown: false, activeTintColor: "#808080", inactiveTintColor: "#FFD700" }}
-                >
-                    <Tab.Screen
-                        initialParams={{isLogado: setIsLogged, isAdmin: setIsAdmin}}
-                        name="Home"
-                        component={ HomeAdminScreen}
-                        options={{
-                            tabBarIcon: ({ color, size }) => <Feather name="home" color={color} size={size} />,
-                        }}
-                    />
-
-                    <Tab.Screen
-                        name="Perfil"
-                        component={ProfileScreen}
-                        options={{
-                            tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
-                        }}
-                    />
-                </Tab.Navigator>
-            </NavigationContainer>
-        ) :
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={{ headerShown: false, activeTintColor: "#808080", inactiveTintColor: "#FFD700" }}
-                >
-                    <Tab.Screen
-                        initialParams={{isLogado: setIsLogged, isAdmin: setIsAdmin}}
-                        name="Home"
-                        component={ HomeClientScreen }
-                        options={{
-                            tabBarIcon: ({ color, size }) => <Feather name="home" color={color} size={size} />,
-                        }}
-                    />
-                    
-                    <Tab.Screen
-                        name="Carrinho"
-                        component={CartScreen}
-                        options={{
-                            tabBarIcon: ({ color, size }) => (
-                                <Feather name="shopping-cart" color={color} size={size} />
-                            ),
-                        }}
-                    />
-
-                    <Tab.Screen
-                        name="Perfil"
-                        component={ProfileScreen}
-                        options={{
-                            tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
-                        }}
-                    />
-                </Tab.Navigator>
-            </NavigationContainer>
-    ) : (
+    return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Login" component={LoginScreen} initialParams={{isLogado: setIsLogged, isAdmin: setIsAdmin}} />
-                <Stack.Screen name="Cadastro" component={SignUpScreen} />
-            </Stack.Navigator>
+            {isLogged ? (
+                isAdmin ? <AdminTabNavigator /> : <ClientTabNavigator />
+            ) : (
+                <AuthNavigator setIsLogged={setIsLogged} setIsAdmin={setIsAdmin} />
+            )}
         </NavigationContainer>
     );
 }
