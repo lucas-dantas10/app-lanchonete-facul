3b80b1e123bdb8a523d6f61d0ab345418e4743b2
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-19 10:07:13 -0300

    Corrijido a conexão com api

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.175:8000/api/v1',
+  baseURL: 'http://localhost:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   }
